ed756447422c7d150265a15825ccc1f70e2618af
Telly Brown <tellybrown@gmail.com>
2019-07-03 13:48:39 -0500

    Working on build

--- build.bat
+++ build.bat
@@ -4,11 +4,14 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
- 
+
 set version=0.1.0
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-
+mkdir Build.AspNet
+mkdir Build.Hosting
 dotnet build PrometheusCore.sln -c %config%
+dotnet pack PrometheusCore.AspNet\PrometheusCore.AspNet.csproj /p:PackageVersion=%version% -o .\..\Build.AspNet
+dotnet pack PrometheusCore.Hosting\PrometheusCore.Hosting.csproj /p:PackageVersion=%version% -o .\..\Build.Hosting
